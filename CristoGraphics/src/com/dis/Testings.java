d64e4be055c808d58ac99c270822477a9067507a
Cristobal Ocampo Quintero <Cristobal Ocampo Quintero>
2016-02-21 02:00:21 -0500

    Arreglando testing

--- CristoGraphics/src/com/dis/Testings.java
+++ CristoGraphics/src/com/dis/Testings.java
@@ -38,9 +38,13 @@ public class Testings {
         Matriz2D res1 = new Matriz2D(aa.multMatriz(a, a));
         Matriz2D res2 = new Matriz2D(aa.sumMatriz(a, res.getMatrix()));
         double res3 = aa.detMatrix(a);
+        System.out.println("Multipliación por Escalar");
         System.out.println(res.toString());
+        System.out.println("Matriz x Matriz");
         System.out.println(res1.toString());
+        System.out.println("Sumando Matriz y Matriz");
         System.out.println(res2.toString());
+        System.out.println("Determinante de la matriz");
         System.out.println(res3);
 
     }
@@ -53,9 +57,13 @@ public class Testings {
         Matriz3D res1 = new Matriz3D(aa.multMatriz(a, a));
         Matriz3D res2 = new Matriz3D(aa.sumMatriz(a, res.getMatrix()));
         double res3 = aa.detMatrix(a);
+        System.out.println("Multipliación por Escalar");
         System.out.println(res.toString());
+        System.out.println("Matriz x Matriz");
         System.out.println(res1.toString());
+        System.out.println("Sumando Matriz y Matriz");
         System.out.println(res2.toString());
+        System.out.println("Determinante de la matriz");
         System.out.println(res3);
     }
 
@@ -67,24 +75,32 @@ public class Testings {
         Vector2D res2 = a.sumarVectores(a, b);
         Vector3D res3 = a.productoCruz(a, b);
         double res4 = a.productoPunto(a, b);
+        System.out.println("Vector 1");
         System.out.println(res1.getX() + "," + res1.getY());
+        System.out.println("Vector 2");
         System.out.println(res2.getX() + "," + res2.getY());
+        System.out.println("Producto cruz v1 x v2");
         System.out.println(res3.getX() + "," + res3.getY() + "," + res3.getZ());
+        System.out.println("Producto punto v1 . v2");
         System.out.println(res4);
     }
 
     public void Vector3D() {
         System.out.println("Testeando Vectores3D");
-        Vector3D c = new Vector3D(1, 3, 5);
-        Vector3D d = new Vector3D(2, 4, 6);
-        Vector3D res5 = c.multVectScalar(c, 2);
-        Vector3D res6 = c.sumarVectores(c, d);
-        Vector3D res7 = c.productoCruz(c, d);
-        double res8 = c.productoPunto(c, d);
-        System.out.println(res5.getX() + "," + res5.getY() + "," + res5.getZ());
-        System.out.println(res6.getX() + "," + res6.getY() + "," + res6.getZ());
-        System.out.println(res7.getX() + "," + res7.getY() + "," + res7.getZ());
-        System.out.println(res8);
+        Vector3D a = new Vector3D(1, 3, 5);
+        Vector3D b = new Vector3D(2, 4, 6);
+        Vector3D res1 = a.multVectScalar(a, 2);
+        Vector3D res2 = a.sumarVectores(a, b);
+        Vector3D res3 = a.productoCruz(a, b);
+        double res4 = a.productoPunto(a, b);
+        System.out.println("Vector 1");
+        System.out.println(res1.getX() + "," + res1.getY() + "," + res1.getZ());
+        System.out.println("Vector 2");
+        System.out.println(res2.getX() + "," + res2.getY() + "," + res2.getZ());
+        System.out.println("Producto cruz v1 x v2");
+        System.out.println(res3.getX() + "," + res3.getY() + "," + res3.getZ());
+        System.out.println("Producto punto v1 . v2");
+        System.out.println(res4);
     }
 
     public void multPuntHMatriz() {
